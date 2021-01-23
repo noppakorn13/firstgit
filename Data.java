f8a80106dc5f482858063bbfdbc01e4b754cb1e7
uncle <everything.alright4@gmail.com>
2021-01-23 10:50:38 +0700

    initial commit

---
+++ Data.java
@@ -0,0 +1,8 @@
+/**
+ * Data
+ */
+public class Data {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
---
+++ Gitnaja.java
@@ -0,0 +1,7 @@
+public class Gitnaja {
+    public static void main(String[] args) {
+        int[] n = new int[2];
+        n[0]=1;
+        n[1]=2;
+    }
+}
